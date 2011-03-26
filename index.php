3ba051cd2861d76578f224507fb3f3deefe140de
Lautaro <orazile@gmail.com>
2011-03-26 13:59:48 -0300

    Example 3

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<h1>jQuery 1.5 Examples</h1>
 		<h2>Deferred Object</h2>
 		<span style="font-weight: bold;">Example 1: </span><a href="example1.php">When -> Then</a><br>
-		<span style="font-weight: bold;">Example 2: </span><a href="example2.php">Simple animation</a>
+		<span style="font-weight: bold;">Example 2: </span><a href="example2.php">Simple animation</a><br>
+		<span style="font-weight: bold;">Example 3: </span><a href="example3.php">Simple animation loop</a>
 	</body>
 </html>
